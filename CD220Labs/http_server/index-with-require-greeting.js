856ec628df1a89123edb4266f222d69cc5bfe188
usmanaryk1 <usmanaryk1@gmail.com>
2024-07-05 18:46:29 +0500

    add greeting file in http server

---
+++ CD220Labs/http_server/index-with-require-greeting.js
@@ -0,0 +1,36 @@
+ // Import the HTTP module
+ const http = require('http');
+
+ // Import the 'today' module
+ const today = require('./today');
+ 
+ // Define the request listener function
+ const requestListener = function (req, res) {
+     res.writeHead(200); // Set the status code to 200 (OK)
+     let dateVal = today.getDate(); // Get the current date from the 'today' module
+ 
+     // Determine the appropriate greeting based on the current time
+     let greeting = "It is still not morning";
+     if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+         greeting = "Good morning!";
+     } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+         greeting = "Good afternoon!";
+     } else if (dateVal.getHours() >= 18 && dateVal.getHours() < 21) {
+         greeting = "Good evening!";
+     } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
+         greeting = "Good night!";
+     }
+ 
+     // Send the response with the appropriate greeting
+     res.end(`Hello, ${greeting}`);
+ };
+ 
+ // Define the port number
+ const port = 8080;
+ 
+ // Create an HTTP server using the request listener function
+ const server = http.createServer(requestListener);
+ 
+ // Start the server and listen on the specified port
+ server.listen(port);
+ console.log('Server listening on port: ' + port);
--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -2,5 +2,9 @@
 module.exports.getDate = function getDate() {
     // Get the current date and time in the timezone "Australia/Brisbane"
     let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+    // return aestTime; // Return the formatted date and time
+
+     // Convert the formatted date and time string back to a Date object
+     let dateObj = new Date(aestTime);
+     return dateObj; // Return the Date object
 };
